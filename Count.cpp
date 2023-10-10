20c9cae0645e4dac6d230e2f13e558f3103d8fe7
abdulrafeysiddiqui <146812735+abdulrafeysiddiqui@users.noreply.github.com>
2023-10-10 12:42:02 +0500

    Add files via upload
    Count

---
+++ Count.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << "-----------------------" << endl;
+	
+	cout << "Abdul Rafey Siddiqui" << endl;
+	
+	cout << "-----------------------" << endl;
+	
+	int arr[15];
+	
+	int count[15] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		
+	cout << "Enter Array's Elements: " << endl;
+	
+	for (int i = 0; i < 15; i++){
+			
+			cin >> arr[i];
+		}
+		
+	for (int i = 0; i < 15; i++){
+		for (int j = 0; j < 15; j++){
+			
+			if (arr[i] == arr[j]){
+				
+				count[i] += 1;
+			}
+		}
+	}
+	
+	for (int i = 0; i < 15; i++){
+		
+		cout << endl;
+		
+		cout << "The " << i+1 << " element has appeared " << count[i] << " times." << endl;
+		
+		cout << endl;
+	}
+	}
